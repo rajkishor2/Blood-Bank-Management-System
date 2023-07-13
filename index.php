aa84660bc8bd55d36727f2cc33cfb0897cd31bb3
rajkishor2 <rajrauniyar9788@gmail.com>
2023-07-13 21:17:37 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Bank Management</title>
+    <!-- Bootstrap files -->
+    <link rel="stylesheet" href="bootstrap/css//bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- External CSS file -->
+    <link rel="stylesheet" href="css/styles.css">
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <a class="navbar-brand" href="index.php">Blood Bank Management System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="admin/login.php">Admin</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="donor/login.php">Donor</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="patient/login.php">Patient</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+<!-- Main content -->
+<div class="container-fluid" style="margin:50px;">
+    <div class="row container" style="text-align: justify;">
+        <div class="col-md-8 mx-auto content-left">
+            <h4>What is Blood Bank Management System?</h4>
+            <p>
+                The Online Blood Bank Management System is a comprehensive solution designed to
+                revolutionize the blood transfusion process and improve the accessibility and efficiency of blood
+                donation and distribution. This system aims to bridge the gap between blood donors, recipients,
+                and healthcare facilities, facilitating seamless coordination and ensuring timely access to
+                life-saving blood units.
+                <br><br>
+                Traditional blood banking systems often face challenges such as limited visibility of available
+                blood units, manual record-keeping, and difficulty in connecting donors with recipients in need.
+                The Online Blood Bank Management System addresses these challenges by providing a
+                centralized platform that streamlines the entire process, from donor registration to recipient
+                blood requests and catalog management.
+            </p>
+            <button class="btn btn-danger">Read more..</button>
+        </div>
+        <div class="col-md-3 content-right">
+            <img class="img-fluid" src="../bbms/image/photo.jpg" height="auto" width="70%">
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10 m-auto" style="text-align: justify;">
+            <h4>What is Blood Bank Management System?</h4>
+            <p>
+                The Online Blood Bank Management System is a comprehensive solution designed to
+revolutionize the blood transfusion process and improve the accessibility and efficiency of blood
+donation and distribution. This system aims to bridge the gap between blood donors, recipients,
+and healthcare facilities, facilitating seamless coordination and ensuring timely access to
+life-saving blood units.
+<br><br>
+Traditional blood banking systems often face challenges such as limited visibility of available
+blood units, manual record-keeping, and difficulty in connecting donors with recipients in need.
+The Online Blood Bank Management System addresses these challenges by providing a
+centralized platform that streamlines the entire process, from donor registration to recipient
+blood requests and inventory management.
+<br><br>
+Through this system, donors can conveniently register their information and schedule blood
+donations, while recipients can search for compatible blood units based on their specific
+requirements. The system also integrates with healthcare facilities, allowing for effective
+communication and coordination between the blood bank and medical professionals.
+
+            </p>
+        </div>
+    </div>
+</div>
+<div class="container-fluid" style="margin-bottom: 5%;">
+    <div class="row">
+        <div class="col-10  m-auto">
+        <h4>Our centers?</h4>
+        <div class="card container">
+            <h4>Kathmandu</h4>
+            <hr>
+            <p>Our center in Kathmandu is equipped with an efficient and advanced blood bank management system. <br>Koteshwor kathmandu Contact : 9741889439</p>
+        </div>
+        <div class="card">
+            <h4>Birgunj</h4>
+            <hr>
+            <p>Our center in Birgunj Parsa is equipped with an efficient and advanced blood bank management system. <br>Radhemai Birgunj  Contact : 9821162140</p>
+        </div>
+        <div class="card">
+            <h4>Pokhara</h4>
+            <hr>
+            <p>Our center in Pokhara Kaski is equipped with an efficient and advanced blood bank management system. <br>Lama Chaur Pokhara Contact : 9807153521</p>
+        </div>
+        <div class="card">
+            <h4>Butwal</h4>
+            <hr>
+            <p>Our center in Butwal Rupandehi is equipped with an efficient and advanced blood bank management system. <br>Bhalwari Butwal Contact : 9741889439</p>
+        </div>
+         <div class="card">
+            <h4>Biratnagar</h4>
+            <hr>
+            <p>Our center in Biratnagar  is equipped with an efficient and advanced blood bank management system. <br> Powerhouse Biratnagar  Contact : 9817220249</p>
+        </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 bg-danger footer">
+            @CopyrightRajSudip.com
+        </div>
+    </div>
+</div>
+</body>
+</html>
